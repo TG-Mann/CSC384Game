7e05435b87499277deead25318862f03ca89b653
tom <118373589+TG-Mann@users.noreply.github.com>
2024-04-08 11:56:14 +0100

    Can enable disable tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -6,9 +6,11 @@ public class Tutorial : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] tutorialMessages;
-    private int tutorialStep = 6;
+    private int tutorialStep;
 
     [SerializeField] Player player;
+
+    bool playerTutorial = true; // to enable / disable tutorial
 
     bool changeMessage = true;
 
@@ -21,83 +23,93 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (changeMessage){
-            for (int i = 0; i < tutorialMessages.Length; i++){
-                if (i == tutorialStep){
-                    tutorialMessages[i].SetActive(true);
-                } else{
-                    tutorialMessages[i].SetActive(false);
+        if (playerTutorial){
+
+            if (changeMessage){
+                for (int i = 0; i < tutorialMessages.Length; i++){
+                    if (i == tutorialStep){
+                        tutorialMessages[i].SetActive(true);
+                    } else{
+                        tutorialMessages[i].SetActive(false);
+                    }
                 }
             }
-        }
        
 
-        if (tutorialStep == 0){
-            if (player.getPLayerState() == "Walk"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 0){
+                if (player.getPLayerState() == "Walk"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
+            } 
+            if (tutorialStep == 1){
+                if (player.getPLayerState() == "Jump"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
+            } 
+            if (tutorialStep == 2){
+                if (player.getPLayerState() == "DoubleJump"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
             }
-        } 
-        if (tutorialStep == 1){
-            if (player.getPLayerState() == "Jump"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 3){
+                if (player.getPLayerState() == "Block"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
             }
-        } 
-        if (tutorialStep == 2){
-            if (player.getPLayerState() == "DoubleJump"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 4){
+                if (player.getPLayerState() == "Roll"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
             }
-        }
-        if (tutorialStep == 3){
-            if (player.getPLayerState() == "Block"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 5){
+                if (player.getPLayerState() == "Dash"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
             }
-        }
-        if (tutorialStep == 4){
-            if (player.getPLayerState() == "Roll"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 6){
+                if (player.getPLayerState() == "AttackOne" || player.getPLayerState() == "AttackTwo"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
             }
-        }
-        if (tutorialStep == 5){
-            if (player.getPLayerState() == "Dash"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 7){
+                if (player.getPLayerState() == "AttackAir"){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                }
             }
-        }
-        if (tutorialStep == 6){
-            if (player.getPLayerState() == "AttackOne" || player.getPLayerState() == "AttackTwo"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
+            if (tutorialStep == 8){
+                tutorialWall.SetActive(false);
+                signPost.SetActive(true);
+                if (player.transform.position.x > -10){
+                    StartCoroutine(Wait());
+                    tutorialStep++;
+                    changeMessage = false;
+                    startGame();
+                }
             }
-        }
-        if (tutorialStep == 7){
-            if (player.getPLayerState() == "AttackAir"){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
-            }
-        }
-        if (tutorialStep == 8){
-             tutorialWall.SetActive(false);
-             signPost.SetActive(true);
+        } else{
+            tutorialWall.SetActive(false);
+            signPost.SetActive(true);
             if (player.transform.position.x > -10){
-                StartCoroutine(Wait());
-                tutorialStep++;
-                changeMessage = false;
                 startGame();
             }
         }
+        
 
     }
 
